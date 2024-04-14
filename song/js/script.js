a69adfcd64534ce3075a9d7a9c757626b5b5625b
rjzim01 <rjzim01@gmail.com>
2024-04-14 19:46:07 +0600

    3.1_music

--- song/js/script.js
+++ song/js/script.js
@@ -17,103 +17,127 @@ let timer;
 let autoplay = 0;
 let loop = 0;
 
-//let index_no = 10;
 let index_no = 0;
 let Playing_song = false;
 
-//create a audio Element
 let track = document.createElement('audio');
 
-//All songs list
 let All_song = [{
         name: "Only Music",
-        //path: "music/song1.mp3",
-        path: "music/amusic15.mp3",
-        //img: "img/img1.jpg",
+        path: "music/1_theme.mp3",
         img: "img/1_img.png",
         singer: "just wow"
     },
     {
         name: "Majhe Majhe",
-        //path: "music/song5.mp3",
-        path: "music/Majhe_Majhe_Tobo_Arindom.mp3",
+        path: "music/2_Majhe_Majhe_Tobo_Arindom.mp3",
         img: "img/2_img.png",
         singer: "!sad"
     },
     {
         name: "Samajagavana",
-        path: "music/song3.mp3",
+        path: "music/3_samajagavana.mp3",
         img: "img/3_img.png",
         singer: "telegu"
     },
     {
         name: "Evaly",
-        //path: "music/song4.mp3",
-        path: "music/Evaly_-_We_Care.mp3",
+        path: "music/4_evaly.mp3",
         img: "img/4_img.png",
         singer: "!marketing"
     },
     {
         name: "Sundor Konna",
-        //path: "music/song2.mp3",
-        path: "music/Sundor_Konna_5.mp3",
+        path: "music/5_sundor_konna.mp3",
         img: "img/5.jpg",
         singer: "!important"
     },
     {
         name: "Dhoro Jodi",
-        //path: "music/song2.mp3",
-        path: "music/DhoroJodi.mp3",
+        path: "music/6_dhorojodi.mp3",
         img: "img/6_dhoro.png",
         singer: "!Sad"
     },
     {
         name: "Lamborgini",
-        //path: "music/song2.mp3",
         path: "music/7_lamborgini.mp3",
         img: "img/7_lamborgini.jpg",
         singer: "!Strong"
     },
     {
         name: "Srotoshinni",
-        //path: "music/song2.mp3",
-        path: "music/Srotoshinni.mp3",
+        path: "music/8_srotoshini.mp3",
         img: "img/8_Srotoshinni.png",
         singer: "!Strong"
     },
     {
         name: "jannat",
-        //path: "music/song2.mp3",
-        path: "music/jannat.mp3",
+        path: "music/9_jannat.mp3",
         img: "img/9_jannat.jpg",
         singer: "forLove"
     },
     {
         name: "jokhon",
-        //path: "music/song2.mp3",
-        path: "music/JokhonPorbeNaMor.mp3",
+        path: "music/10_jokhon_porbe_na_mor.mp3",
         img: "img/10_jodiAmar.png",
         singer: "Revealed"
     },
     {
         name: "BeSuccessFull",
-        //path: "music/song2.mp3",
-        path: "music/music2.mp3",
+        path: "music/11_theme.mp3",
         img: "img/11_music2.png",
         singer: "AchieveTheGoal"
     },
     {
         name: "LoL",
-        //path: "music/song2.mp3",
-        path: "music/lol.mp3",
+        path: "music/12_lol.mp3",
         img: "img/12_lol.jpg",
         singer: "AchieveTheGoal"
-    }        
+    },
+    {
+        name: "Gangstar",
+        path: "music/13_gangstar.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    },
+    {
+        name: "khoparBadhon",
+        path: "music/14_Alga_korogo_khopar_badhon.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    },
+    {
+        name: "Komolay",
+        path: "music/15_Komolay_Nrityo_Kore.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    },
+    {
+        name: "Butapomma",
+        path: "music/16_butapomma.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    },
+    {
+        name: "Ramulo",
+        path: "music/17_ramulo.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    },
+    {
+        name: "Dhundne",
+        path: "music/18_dhundne.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    },
+    {
+        name: "Abaro",
+        path: "music/19_abaro.mp3",
+        img: "img/12_lol.jpg",
+        singer: "AchieveTheGoal"
+    }
 ];
 
-// All functions
-
-// function load the track
 function load_track(index_no) {
     clearInterval(timer);
     reset_slider();
@@ -131,14 +155,12 @@ function load_track(index_no) {
 
 load_track(index_no);
 
-//mute sound function
 function mute_sound() {
     track.volume = 0;
     volume.value = 0;
     volume_show.innerHTML = 0;
 }
 
-// checking.. the song is playing or not
 function justplay() {
     if (Playing_song == false) {
         playsong();
@@ -148,26 +170,22 @@ function justplay() {
     }
 }
 
-// reset song slider
 function reset_slider() {
     slider.value = 0;
 }
 
-// play song
 function playsong() {
     track.play();
     Playing_song = true;
     play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
 }
 
-//pause song
 function pausesong() {
     track.pause();
     Playing_song = false;
     play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
 }
 
-// next song
 function next_song() {
     if (index_no < All_song.length - 1) {
         index_no += 1;
@@ -180,7 +198,6 @@ function next_song() {
     }
 }
 
-// previous song
 function previous_song() {
     if (index_no > 0) {
         index_no -= 1;
@@ -194,19 +211,16 @@ function previous_song() {
     }
 }
 
-// change volume
 function volume_change() {
     volume_show.innerHTML = recent_volume.value;
     track.volume = recent_volume.value / 100;
 }
 
-// change slider position 
 function change_duration() {
     slider_position = track.duration * (slider.value / 100);
     track.currentTime = slider_position;
 }
 
-// autoplay function
 function autoplay_switch() {
     if (autoplay == 1) {
         autoplay = 0;
@@ -220,13 +234,11 @@ function autoplay_switch() {
 function range_slider() {
     let position = 0;
 
-    // update slider position
     if (!isNaN(track.duration)) {
         position = track.currentTime * (100 / track.duration);
         slider.value = position;
     }
 
-    // function will run when the song is over
     if (track.ended) {
         play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
         if (autoplay == 1) {
@@ -236,10 +248,6 @@ function range_slider() {
         }
     }
 }
-
-// function enableLoop() { 
-//   track.loop = true;
-// } 
 
 function enableLoop() { 
     if (loop == 1) {
@@ -255,17 +263,3 @@ function enableLoop() {
     }
 } 
 
-// function disableLoop() { 
-//   track.loop = false;
-// }
-
-// autoplay function
-// function autoplay_switch() {
-//     if (autoplay == 1) {
-//         autoplay = 0;
-//         auto_play.style.background = "rgba(255,255,255,0.2)";
-//     } else {
-//         autoplay = 1;
-//         auto_play.style.background = "#FF8A65";
-//     }
-// }
